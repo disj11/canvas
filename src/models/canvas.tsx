28aa3f9d853b2046bf48f7db2b951c1cba55e7eb
disj11 <disj11@naver.com>
2020-12-24 00:48:05 +0900

    init

---
+++ src/models/canvas.tsx
@@ -0,0 +1,22 @@
+import { fabric } from "fabric";
+
+export class FabricCanvas {
+    private readonly canvas: fabric.Canvas;
+    
+    constructor(id: string, options?: fabric.ICanvasOptions) {
+        this.canvas = new fabric.Canvas(id, options)
+    }
+
+    public addRect() {
+        this.canvas.add(new fabric.Rect({
+            width: 30,
+            height: 30,
+        }));
+    }
+
+    public destory() {
+        if (this.canvas) {
+            this.canvas.dispose();
+        }
+    }
+}
