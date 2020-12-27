0eb6ffbfa732c04ea420b68d8c0f8e6182547668
tykim <disj11@naver.com>
2020-12-28 02:35:13 +0900

    텍스트 이벤트 오류 수정

--- src/models/canvas/TextCanvasEvent.tsx
+++ src/models/canvas/TextCanvasEvent.tsx
@@ -1,5 +1,6 @@
-import fabric from "fabric/fabric-impl";
+import { fabric } from "fabric";
 import { CanvasEventHandler, CanvasEventObject } from "./CanvasEventModel";
+import { ToolTypes } from "./ToolTypes";
 
 interface TextCanvasEvent extends CanvasEventObject {
     object: fabric.IText,
@@ -7,13 +8,19 @@ interface TextCanvasEvent extends CanvasEventObject {
 
 export class TextCanvasMouseUpEvent implements CanvasEventHandler<TextCanvasEvent> {
     handle(e: TextCanvasEvent): void {
+        e.object = new fabric.IText("텍스트를 입력해주세요", {
+            top: e.currentCursorPosition.y,
+            left: e.currentCursorPosition.x,
+            fontSize: 20,
+        });
+
         e.canvasStore.canvas.add(e.object);
+        e.canvasStore.selectedTool = ToolTypes.SELECT;
     }
 }
 
 export class TextCanvasMouseDownEvent implements CanvasEventHandler<TextCanvasEvent> {
     handle(e: TextCanvasEvent): void {
-        e.object = new fabric.IText("텍스트를 입력해주세요");
     }
 }
 
