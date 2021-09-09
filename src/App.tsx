913bc702b691289d33f2c4c1db2986b4ab7d4224
disj11 <disj11@naver.com>
2021-09-09 23:16:26 +0900

    refactor: 테마 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import {StoreProvider} from "./contexts/storeContext";
 import rootStore from "./stores/rootStore";
 import {CssBaseline} from "@material-ui/core";
-import {ThemeProvider} from "./theme";
 
 const App = () => {
     React.useEffect(() => {
@@ -11,12 +10,12 @@ const App = () => {
     }, []);
 
     return (
-        <ThemeProvider>
+        <React.Fragment>
             <CssBaseline />
             <StoreProvider value={rootStore}>
                 <Canvas/>
             </StoreProvider>
-        </ThemeProvider>
+        </React.Fragment>
     );
 }
 
